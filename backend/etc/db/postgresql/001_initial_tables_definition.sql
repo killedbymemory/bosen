432e1e2fe2ae58faa848985abd49504e38b3351c
Leonardo Situmorang <leonardo@situmorang.net>
2018-10-16 23:11:11 +0700

    Add first, middle, and last name fields in userinfo

--- backend/etc/db/postgresql/001_initial_tables_definition.sql
+++ backend/etc/db/postgresql/001_initial_tables_definition.sql
@@ -2,7 +2,10 @@ CREATE TABLE userinfo
 (
   uid uuid NOT NULL,
   username character varying(100) NOT NULL,
-  password character varying(100) NOT NULL
+  password character varying(100) NOT NULL,
+  firstName character varying(100) NOT NULL,
+  middleName character varying(100) NULL,
+  lastName character varying(100) NULL
 )
 WITH(OIDS=FALSE);
 
